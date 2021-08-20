4121e36b63ebcb447fdb56ae09b67857052a67b2
Saumya Mundra <81912330+Saumya-svm@users.noreply.github.com>
2021-08-21 00:35:43 +0530

    Update CF_63a.py

--- CF_63a.py
+++ CF_63a.py
@@ -13,17 +13,30 @@ for i in range(l[4],l[5]+1):
     c+=1
 print(c)
 
+
+
 #efficent soutions to learn from
+
 #1
 *P, a, b = map(int, input().split())
  
 print(max(0, min(b-a+1, min(P)-a)))
+
+
+
+
+
+
 #2
 a,b,c,d,e,f=map(int,input().split())
 b=min(a,b,c,d)
 if b>f:
     b=f+1
 print(len([1 for i in range(e,b)]))
+
+
+
+
 #3
 p,q,r,s,a,b = input().split()
 p,q,r,s,a,b = [int(p),int(q),int(r),int(s),int(a),int(b)]
